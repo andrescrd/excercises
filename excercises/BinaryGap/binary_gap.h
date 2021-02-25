8d6c7a0cfb181189dde49a9d2b0a26d5e69fa83e
andrescrd <c.andresricardo@gmail.com>
2021-02-25 17:28:09 -0300

    adding binary gap

---
+++ excercises/BinaryGap/binary_gap.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <xstring>
+
+class binary_gap
+{
+protected:
+    static std::string get_binary(int num);
+public:
+
+    static int get_max_gap(const int num);
+};
--- excercises/excercises.cpp
+++ excercises/excercises.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+#include <ostream>
+#include "BinaryGap/binary_gap.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    
+    std::cout << "Gap 9: " << binary_gap::get_max_gap(9) << std::endl;
+    std::cout << "Gap 529: " << binary_gap::get_max_gap(529) << std::endl;
+    std::cout << "Gap 20: " << binary_gap::get_max_gap(20) << std::endl;
+    std::cout << "Gap 15: " << binary_gap::get_max_gap(15) << std::endl;
+    std::cout << "Gap 32: " << binary_gap::get_max_gap(32) << std::endl;
+    std::cout << "Gap 1041: " << binary_gap::get_max_gap(1041) << std::endl;
+
     return 0;
 }
