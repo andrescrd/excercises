a7664113948c1b54144d21f923a849135f8a2699
andrescrd <c.andresricardo@gmail.com>
2021-02-25 19:29:25 -0300

    refactoring

---
+++ PermMissingElem/perm_missing_elem.cpp
@@ -0,0 +1,23 @@
+﻿#include "perm_missing_elem.h"
+
+#include <algorithm>
+
+int perm_missing_elem::solution(std::vector<int>& A)
+{
+    if (A.empty())
+        return 1;
+
+    std::sort(A.begin(), A.end());
+
+    int num = A[0];
+    
+    for (int i = 0; i < A.size(); ++i)
+    {
+        if(A[i] != num)
+            return num;
+
+        num++;
+    }
+
+    return num;
+}
