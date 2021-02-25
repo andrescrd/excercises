e0635d05a9116690975068a39abe7e0fab23ef6d
andrescrd <c.andresricardo@gmail.com>
2021-02-25 18:30:38 -0300

    OddOccurrencesInArray

--- excercises/OddOccurrencesInArray/odd_occurrences.cpp
+++ excercises/OddOccurrencesInArray/odd_occurrences.cpp
@@ -5,7 +5,7 @@ int odd_occurrences::solution(std::vector<int>& A)
     const int size = A.size();
     int sum = 0;
     
-    for (unsigned int i=0; i<size; i++) {
+    for (int i=0; i<size; i++) {
         sum ^= A[i];
     }
     
