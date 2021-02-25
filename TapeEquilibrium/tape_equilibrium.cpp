03432dde67b118efc0b84ec500739e1702c60df8
andrescrd <c.andresricardo@gmail.com>
2021-02-25 20:08:07 -0300

    TapeEquilibrium

---
+++ TapeEquilibrium/tape_equilibrium.cpp
@@ -0,0 +1,25 @@
+﻿#include "tape_equilibrium.h"
+
+int tape_equilibrium::solution(std::vector<int>& A)
+{
+    if (A.empty())
+        return 0;
+
+    int right = 0;
+    int left = A[0];
+    const int size = A.size();
+
+    for (int i = 1; i < size; ++i)
+        right += A[i];
+
+    int ans = abs(left - right);
+    
+    for (int i = 1; i < size; ++i)
+    {
+        ans = abs(left - right) < ans ? abs(left - right) : ans;
+        left += A[i];
+        right -= A[i];
+    }
+
+    return ans;
+}
