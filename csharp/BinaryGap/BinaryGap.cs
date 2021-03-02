4497f35b2af6ef1eaff884ab4472483f318d2902
andrescrd <c.andresricardo@gmail.com>
2021-03-02 16:29:50 -0300

    binary gap csharp

---
+++ csharp/BinaryGap/BinaryGap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public static class BinaryGap
+{
+    public static int Solution(int n)
+    {
+        if (n <= 0)
+            return 0;
+
+        int gap = 0;
+        int maxGap = 0;
+        var binary = Convert.ToString(n, 2).ToCharArray();
+
+        for (int i = 1; i < binary.Length; i++)
+        {
+            if (binary[i] == '1')
+            {
+                if (gap > maxGap)
+                    maxGap = gap;
+                
+                gap = 0;
+            }
+            else
+            {
+                gap++;
+            }
+        }
+
+        return maxGap;
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine($"Gap is {BinaryGap.Solution(9)}");
+            Console.WriteLine($"Gap is {BinaryGap.Solution(529)}");
+            Console.WriteLine($"Gap is {BinaryGap.Solution(20)}");
+            Console.WriteLine($"Gap is {BinaryGap.Solution(15)}");
+            Console.WriteLine($"Gap is {BinaryGap.Solution(32)}");
+            Console.WriteLine($"Gap is {BinaryGap.Solution(1041)}");
+        }
+    }
+}
