88c558e4b5497f227594d6d10cc9e9ccc81e84e3
andrescrd <c.andresricardo@gmail.com>
2021-03-02 16:50:41 -0300

    rotation with recursion

---
+++ csharp/CyclicRotation/CyclicRotation.cs
@@ -0,0 +1,16 @@
+﻿public static class CyclicRotation
+{
+    public static int[] Solution(int[] a, int k)
+    {
+        if (a.Length <= 0 || k <= 0)
+            return a;
+
+        var b = new int[a.Length];
+        b[0] = a[^1];
+
+        for (int i = 0; i < a.Length - 1; i++)
+            b[i + 1] = a[i];
+
+        return k > 1 ? Solution(b, k - 1) : b;
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -12,6 +12,12 @@ namespace excercises
             Console.WriteLine($"Gap is {BinaryGap.Solution(15)}");
             Console.WriteLine($"Gap is {BinaryGap.Solution(32)}");
             Console.WriteLine($"Gap is {BinaryGap.Solution(1041)}");
+            
+            Console.WriteLine(Environment.NewLine);
+
+            var result = CyclicRotation.Solution(new[] {1, 2, 3, 4}, 2);
+            Console.WriteLine($"Rotation: {string.Join('-',result)}");
+
         }
     }
 }
