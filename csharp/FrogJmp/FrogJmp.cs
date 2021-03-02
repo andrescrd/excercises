b0ccfdf407abf7349853dee46fbebcfc65e1a5be
andrescrd <c.andresricardo@gmail.com>
2021-03-02 19:24:44 -0300

    FrogJmp

---
+++ csharp/FrogJmp/FrogJmp.cs
@@ -0,0 +1,8 @@
+﻿public static class FrogJmp
+{
+    public static int Solution(int x, int y, int d)
+    {
+        var distance = y - x;
+        return (distance + d - 1) / d;
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -11,14 +11,18 @@ namespace excercises
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(20)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(15)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(32)}");
-            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(1041)}");
-            
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(1041)}");            
             Console.WriteLine(Environment.NewLine);
+            
             var result = CyclicRotation.Solution(new[] {1, 2, 3, 4}, 2);
-            Console.WriteLine($"CyclicRotation: {string.Join('-',result)}");    
-            
+            Console.WriteLine($"CyclicRotation: {string.Join('-',result)}");
             Console.WriteLine(Environment.NewLine);
+            
             Console.WriteLine($"OddOccurrencesInArray: {OddOccurrencesInArray.Solution(new[] {1, 1,2,2, 3, 4,4})}");
+            Console.WriteLine(Environment.NewLine);
+            
+            Console.WriteLine($"FrogJmp: {FrogJmp.Solution(10,85,30)}");
+            Console.WriteLine(Environment.NewLine);
 
         }
     }
