231e82068036ac9c86a8e21a22be1e4712401c29
andrescrd <c.andresricardo@gmail.com>
2021-03-02 19:37:32 -0300

    PermMissingElem:

---
+++ csharp/PermMissingElem/PermMissingElem.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+public static class PermMissingElem
+{
+    public static int Solution(int[] a)
+    {
+        if (a.Length <= 0) return 0;
+
+        var orderedEnumerable = a.OrderBy(r => r).ToArray();
+
+        for (int i = 0; i < orderedEnumerable.Length; i++)
+        {
+            if (i + 1 != orderedEnumerable[i])
+                return i + 1;
+        }
+
+        return 0;
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -5,25 +5,27 @@ namespace excercises
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(9)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(529)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(20)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(15)}");
             Console.WriteLine($"BinaryGap is {BinaryGap.Solution(32)}");
-            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(1041)}");            
-            Console.WriteLine(Environment.NewLine);
-            
-            var result = CyclicRotation.Solution(new[] {1, 2, 3, 4}, 2);
-            Console.WriteLine($"CyclicRotation: {string.Join('-',result)}");
-            Console.WriteLine(Environment.NewLine);
-            
-            Console.WriteLine($"OddOccurrencesInArray: {OddOccurrencesInArray.Solution(new[] {1, 1,2,2, 3, 4,4})}");
-            Console.WriteLine(Environment.NewLine);
-            
-            Console.WriteLine($"FrogJmp: {FrogJmp.Solution(10,85,30)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(1041)}");
             Console.WriteLine(Environment.NewLine);
 
+            var result = CyclicRotation.Solution(new[] {1, 2, 3, 4}, 2);
+            Console.WriteLine($"CyclicRotation: {string.Join('-', result)}");
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"OddOccurrencesInArray: {OddOccurrencesInArray.Solution(new[] {1, 1, 2, 2, 3, 4, 4})}");
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"FrogJmp: {FrogJmp.Solution(10, 85, 30)}");
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"PermMissingElem: {PermMissingElem.Solution(new[] {6, 7, 8, 1, 2, 4, 5})}");
+            Console.WriteLine(Environment.NewLine);
         }
     }
 }
