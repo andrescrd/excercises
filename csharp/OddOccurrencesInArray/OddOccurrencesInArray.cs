e57114174ea9ed509c864bace751bc473bcb8024
andrescrd <c.andresricardo@gmail.com>
2021-03-02 16:56:45 -0300

    OddOccurrencesInArray

---
+++ csharp/OddOccurrencesInArray/OddOccurrencesInArray.cs
@@ -0,0 +1,12 @@
+﻿public static class OddOccurrencesInArray
+{
+    public static int Solution(int[] a)
+    {
+        var result = 0;
+
+        for (int i = 0; i < a.Length; i++)
+            result ^= a[i];
+
+        return result;
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -6,17 +6,19 @@ namespace excercises
     {
         static void Main(string[] args)
         {            
-            Console.WriteLine($"Gap is {BinaryGap.Solution(9)}");
-            Console.WriteLine($"Gap is {BinaryGap.Solution(529)}");
-            Console.WriteLine($"Gap is {BinaryGap.Solution(20)}");
-            Console.WriteLine($"Gap is {BinaryGap.Solution(15)}");
-            Console.WriteLine($"Gap is {BinaryGap.Solution(32)}");
-            Console.WriteLine($"Gap is {BinaryGap.Solution(1041)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(9)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(529)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(20)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(15)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(32)}");
+            Console.WriteLine($"BinaryGap is {BinaryGap.Solution(1041)}");
             
             Console.WriteLine(Environment.NewLine);
-
             var result = CyclicRotation.Solution(new[] {1, 2, 3, 4}, 2);
-            Console.WriteLine($"Rotation: {string.Join('-',result)}");
+            Console.WriteLine($"CyclicRotation: {string.Join('-',result)}");    
+            
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"OddOccurrencesInArray: {OddOccurrencesInArray.Solution(new[] {1, 1,2,2, 3, 4,4})}");
 
         }
     }
